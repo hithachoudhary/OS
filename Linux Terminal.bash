5910047b07872efbc182a8df9f2fd19c299a3c32
hithachoudhary <128136109+hithachoudhary@users.noreply.github.com>
2023-06-09 14:48:54 +0530

    Create Linux Terminal.bash

---
+++ Linux Terminal.bash
@@ -0,0 +1,33 @@
+echo "Hello World";
+
+#! prime number
+#!/bin/bash
+echo -e "Enter Number : \c"
+read n
+for((i=2; i<=$n/2; i++))
+do
+  ans=$(( n%i ))
+  if [ $ans -eq 0 ]
+  then
+    echo "$n is not a prime number."
+    exit 0
+  fi
+done
+echo "$n is a prime number."
+
+
+#! reverse of a number
+#!/bin/bash
+echo "Enter a Number:"
+read a
+rev=0
+sd=0
+or=$a
+while [ $a -gt 0 ]
+do
+sd=`expr $a % 10`
+temp=`expr $rev \* 10`
+rev=`expr $temp + $sd`
+a=`expr $a / 10`
+done
+echo "Reverse of $or is $rev"
